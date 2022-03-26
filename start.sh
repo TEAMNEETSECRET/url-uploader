f0d8d58e491acd781f933e09cbe8d61865f55ced
TEAMNEETSECRET <102409055+TEAMNEETSECRET@users.noreply.github.com>
2022-03-26 14:40:49 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@ echo "Cloning Repo...."
 git clone https://github.com/Tellybots/Uploader-Bot /Uploader-Bot
 cd /Uploader-Bot
 pip3 install -U -r requirements.txt
-echo "Starting Bot.... Please Wait. Check You Log. Bot Credit @DKBOTZ\n Special Credit :- @Tellybots"
+echo "Starting Bot.... Please Wait. Check You Log. Bot Credit @TNS_BOTS\n Special Credit :- @TNS_BOTS"
 python3 bot.py
